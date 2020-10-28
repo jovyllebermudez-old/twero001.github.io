a89268d7f0dac208770fc9788180cea2f449840f
jovylle bermudez taga lapu lapu city <jovyllebermudez@gmail.com>
2020-10-28 17:27:30 +0800

    init commit

---
+++ precache-manifest.0fe7d7661654f1f321bd82535fb403b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9374e026d41761c29bc13dd4e484d19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5c60e0bbc165ff20f35",
+    "url": "/static/css/main.ad7b258b.chunk.css"
+  },
+  {
+    "revision": "381e98577244344d98bd",
+    "url": "/static/js/2.5b2ab0ae.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.5b2ab0ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5c60e0bbc165ff20f35",
+    "url": "/static/js/main.b06c9b8f.chunk.js"
+  },
+  {
+    "revision": "bc2047681e9fec440a33",
+    "url": "/static/js/runtime-main.b916b8d9.js"
+  }
+]);
